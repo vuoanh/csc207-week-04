6228f600519fe13dffda9e8fec7d4a0aa3d3204e
Thu Nguyen <nguyenth1@cs.grinnell.edu>
2015-09-27 14:51:09 -0500

    Documentation added for ant

--- Ant.java
+++ Ant.java
@@ -1,8 +1,7 @@
 import java.awt.Color;
 
 /**
- * An ant alternates walking north and east if StepsNorth equal to true, otherwise an
- * ant alternates walking south and west,
+ * An ant either moves north and east or south and west depending on boolean stepsNorth
  */
 public class Ant extends Critter {
 	private boolean stepsNorth;
@@ -29,19 +28,28 @@ public class Ant extends Critter {
 			else {return Direction.WEST;} 
 		}
 	}	
+	
+	/** @return the food type for this Ant: grass */
 	public FoodType getFoodType() {
 		return FoodType.GRASS;
 	}
+	
+	/** @returns the color for this Ant: black */
 	public Color getColor() {
 		return Color.BLACK;
 	}
+	
+	/** @returns the speed of this Ant: fast*/
 	public Speed getSpeed() {
 		return Speed.FAST;
-	}
+	}	
+	
+	/** @returns the String representation of this Ant */
 	public String toString() {
 		return "%";
 	}
 
+	/** @returns either true of false for stepsNorth */
 	public boolean getStepsNorth(){
 		return stepsNorth;
 	}
